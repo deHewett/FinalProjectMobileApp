43f918b70702577ca21d6e59cd9f85ccfb3a3135
deHewett <42918243+deHewett@users.noreply.github.com>
2019-09-03 08:06:54 +1200

    user model

---
+++ FinalMobileProject/FinalMobileProject/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalMobileProject.Models
+{
+    class User
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string ContactNumber { get; set; }
+        public string BillingAddress { get; set; }
+        public string DeliveryAddress { get; set; }
+        public string UserType { get; set; }
+    }
+}
